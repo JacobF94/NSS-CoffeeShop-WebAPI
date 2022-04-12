d3ad174ce0d4d04e37ace2f76bbc6123dd057f17
JacobF94 <jacob.frensley@gmail.com>
2022-04-11 21:44:55 -0500

    day one progress

--- js-app/main.js
+++ js-app/main.js
@@ -1,4 +1,5 @@
 const url = "https://localhost:5001/api/beanvariety/";
+const coffeeUrl = "https://localhost:5001/api/coffee/";
 
 const button = document.querySelector("#run-button");
 button.addEventListener("click", () => {
@@ -8,6 +9,18 @@ button.addEventListener("click", () => {
         })
 });
 
+const coffeeButton = document.querySelector("#coffee-button");
+coffeeButton.addEventListener("click", () => {
+    getAllCoffees()
+        .then(coffees => {
+            console.log(coffees);
+        })
+})
+
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+function getAllCoffees() {
+    return fetch(coffeeUrl).then(resp => resp.json());
 }
